1db7c451a2e90b3b8aed16c3212fa64e0772f25e
superkhung <ogusers.gg@gmail.com>
2024-10-23 00:03:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     <meta name="msapplication-TileColor" content="#da532c">
     <meta name="theme-color" content="#ffffff">
 
-    <title>LOL2D</title>
+    <title>vnLoader |  Liên Minh Huyền Thoại</title>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
         integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
